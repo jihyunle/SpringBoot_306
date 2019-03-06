21d995c1912d4fc68bffa8b794477eb2006726d0
jihyunle <jennifer.jlee@outlook.com>
2019-03-06 17:44:20 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    DirectorRepository directorRepository;
+
+    @RequestMapping("/")
+    public String index(Model model){
+        // create a director
+        Director director = new Director();
+        director.setName("Stephen Bullock");
+        director.setGenre("Sci Fi");
+
+        // create a movie
+        Movie movie = new Movie();
+        movie.setTitle("Star Movie");
+        movie.setYear(2017);
+        movie.setDescription("About Stars...");
+
+        // add the movie to an empty list
+        Set<Movie> movies = new HashSet<Movie>();
+        movies.add(movie);
+
+        // add another movie
+        movie = new Movie();
+        movie.setTitle("DeathStar Ewoks");
+        movie.setYear(2011);
+        movie.setDescription("About Ewoks on the DeathStar...");
+        movies.add(movie);
+
+        // add the list of movies to the director's movie list
+        director.setMovies(movies);
+
+        // save the director to the database
+        directorRepository.save(director);
+
+        // grab all the directors from the db and sent them to the template
+        model.addAttribute("directors", directorRepository.findAll());
+
+        return "index";
+    }
+
+}
